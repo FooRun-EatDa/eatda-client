9b3150e3127c07dd943564b6573816469752c705
k-nh <skgml977@gmail.com>
2022-01-26 14:59:45 +0900

    디자인생성🪄 #3 : 하단 NavigationBar 생성

---
+++ EatDa/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarController.swift
+//  EatDa
+//
+//  Created by 김나희 on 2022/01/26.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        let homeViewController = UIViewController()
+        homeViewController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(imageLiteralResourceName: "home"), selectedImage: UIImage(imageLiteralResourceName: "home_fill"))
+        let likeViewController = UIViewController()
+        likeViewController.tabBarItem = UITabBarItem(title: "좋아요", image: UIImage(imageLiteralResourceName: "heart"), tag: 1)
+        let communityViewController = UIViewController()
+        communityViewController.tabBarItem = UITabBarItem(title: "맛집 톡", image: UIImage(imageLiteralResourceName: "talk"), tag: 2)
+        let eventViewController = UIViewController()
+        eventViewController.tabBarItem = UITabBarItem(title: "이벤트", image: UIImage(imageLiteralResourceName: "event"), tag: 3)
+        let mypageViewController = UIViewController()
+        mypageViewController.tabBarItem = UITabBarItem(title: "마이", image: UIImage(imageLiteralResourceName: "my"), tag: 4)
+        
+        viewControllers = [homeViewController, likeViewController, communityViewController, eventViewController, mypageViewController]
+        
+    }
+
+
+}
+
