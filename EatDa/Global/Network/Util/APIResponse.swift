4fbfc0b761941ca2f74177ad1013a337e55c0fac
k-nh <skgml977@gmail.com>
2022-05-29 14:36:46 +0900

    기능추가🔧 #40: request Method별 encoding 설정

--- EatDa/Global/Network/Util/APIResponse.swift
+++ EatDa/Global/Network/Util/APIResponse.swift
@@ -33,7 +33,9 @@ class API<T: Decodable> {
     
     let headers: HTTPHeaders = [
         "Content-Type":"application/json",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "Authorization": TokenUtils.read(key: Const.KeyChainKey.accessToken) ?? "",
+        "X-Refresh-Token": TokenUtils.read(key: Const.KeyChainKey.refreshToken) ?? ""
     ]
     
     init (url: String, method: HTTPMethod, parameters: Parameters) {
@@ -43,13 +45,31 @@ class API<T: Decodable> {
     }
     
     func fetch(completion: @escaping (APIResponse<T>) -> Void) {
+        var encodingType: ParameterEncoding {
+            switch self.method {
+            case .get, .delete:
+                return URLEncoding.default
+            case .post, .put:
+                return JSONEncoding.default
+            default:
+                return URLEncoding.default
+            }
+        }
+        
         AF.request(self.fetchURL,
                    method: self.method,
                    parameters: self.parameters,
-                   encoding: URLEncoding.default,
+                   encoding: encodingType,
                    headers: headers)
         .validate(statusCode: 200..<300)
         .responseJSON { response in
+            // 토큰 설정
+            guard let accessToken = response.response?.allHeaderFields["Authorization"] as? String else { return }
+            guard let refreshToken = response.response?.allHeaderFields["X-Refresh-Token"] as? String else { return }
+            // 토큰 키체인에 저장
+            TokenUtils.create(key: Const.KeyChainKey.accessToken, token: accessToken)
+            TokenUtils.create(key: Const.KeyChainKey.refreshToken, token: refreshToken)
+            
             switch response.result {
             case .success(let value):
                 
@@ -68,41 +88,5 @@ class API<T: Decodable> {
             
         }
     }
-    
-    
-    func postSignInWithToken(completion: @escaping (APIResponse<T>) -> Void) {
-        AF.request(self.fetchURL,
-                   method: self.method,
-                   parameters: self.parameters,
-                   encoding: JSONEncoding.default,
-                   headers: headers)
-        .validate(statusCode: 200..<300)
-        .responseJSON { response in
-            // 토큰 설정
-            guard let accessToken = response.response?.allHeaderFields["Authorization"] as? String else { return }
-            guard let refreshToken = response.response?.allHeaderFields["X-Refresh-Token"] as? String else { return }
-            // 토큰 키체인에 저장
-            
-            TokenUtils.create(key: Const.KeyChainKey.accessToken, token: accessToken)
-            TokenUtils.create(key: Const.KeyChainKey.refreshToken, token: refreshToken)
-                
-            switch response.result {
-            case .success(let value):
-                do {
-                    let jsonData = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
-                    let result = try JSONDecoder().decode(APIResponse<T>.self, from: jsonData)
-                    completion(result)
-                } catch (let err){
-                    print(err.localizedDescription)
-                }
-                
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-            
-        }
-    }
-    
-    
 
 }
