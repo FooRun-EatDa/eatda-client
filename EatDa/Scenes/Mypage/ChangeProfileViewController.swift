fae9030a2364a2225d47c084eddefec700196de5
heejin342 <tktmzp0526@naver.com>
2022-06-07 02:45:06 +0900

    디자인생성🪄: #49 회원용 마이페이지, 닉네임 변경 페이지 UI 생성

--- EatDa/Scenes/Mypage/ChangeProfileViewController.swift
+++ EatDa/Scenes/Mypage/ChangeProfileViewController.swift
@@ -10,10 +10,88 @@ import UIKit
 
 class ChangeProfileViewController: UIViewController {
     
+    private lazy var profileImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .dateGrayColor
+        view.layer.cornerRadius = 50
+        return view
+    }()
+    
+    private lazy var nickLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "닉네임"
+        titleLabel.font = .mySystemFont(ofSize: 14)
+        return titleLabel
+    }()
+    
+    private lazy var nickTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "기존유저의닉네임"
+        textField.textColor = .mainTextColor
+        textField.font = .mySystemFont(ofSize: 14)
+        return textField
+    }()
+
+    private lazy var emailCheckView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray1
+        return view
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setupNavigation()
+        setupUI()
     }
+    
+    func setupUI(){
+        [profileImageView, nickLabel, nickTextField, emailCheckView].forEach { view.addSubview($0) }
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(43)
+            make.width.height.equalTo(100)
+        }
+        
+        nickLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(90)
+            make.leading.equalToSuperview().offset(24)
+            make.width.equalTo(40)
+        }
+        
+        nickTextField.snp.makeConstraints { make in
+            make.centerY.equalTo(nickLabel)
+            make.leading.equalTo(nickLabel.snp.trailing).offset(14)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+        
+        emailCheckView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(nickTextField)
+            make.top.equalTo(nickTextField.snp.bottom).offset(3)
+            make.height.equalTo(1)
+        }
+    }
+    
+    func setupNavigation(){
+        self.navigationItem.title = "프로필 변경"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(self.goBack))
+        self.navigationItem.leftBarButtonItem?.tintColor = .mainTextColor
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "확인", style: .plain, target: self, action: #selector(self.changeInfo))
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.mySystemFont(ofSize: 16)]
+        self.navigationItem.rightBarButtonItem?.tintColor = .applyButtonColor
+
+    }
+    
+    @objc func goBack(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func changeInfo(){
+        print("API 연동 필요")
+    }
+
     
     
 }
--- EatDa/Scenes/Mypage/MypageViewController.swift
+++ EatDa/Scenes/Mypage/MypageViewController.swift
@@ -63,6 +63,10 @@ class MyPageViewController: UIViewController {
         button.rx.tap.bind { [weak self] in
             let vc = ChangeProfileViewController()
             vc.hidesBottomBarWhenPushed = true
+            
+            let backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+            self?.navigationItem.backBarButtonItem = backBarButtonItem
+ 
             self?.navigationController?.pushViewController(vc, animated: true)
         }.disposed(by: disposeBag)
         return button
@@ -140,7 +144,6 @@ class MyPageViewController: UIViewController {
         return button
     }()
 
-
     private lazy var myActivityTitleLable: UILabel = {
         let label = UILabel()
         label.font = .myBoldSystemFont(ofSize: 18)
@@ -148,7 +151,6 @@ class MyPageViewController: UIViewController {
         label.textColor = .mainTextColor
         return label
     }()
-    
     
     private lazy var myActivityPostTitleLable: UILabel = {
         let label = UILabel()
@@ -215,17 +217,22 @@ class MyPageViewController: UIViewController {
     //최근 본 맛집
     private lazy var recentMatzipView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
         return view
     }()
 
+    private lazy var recentMatzipTitleLable: UILabel = {
+        let label = UILabel()
+        label.font = .myBoldSystemFont(ofSize: 18)
+        label.text = "최근 본 맛집"
+        label.textColor = .mainTextColor
+        return label
+    }()
 
     //계정설정
     private lazy var settingHeader: UIView = {
         let view = UIView()
         return view
     }()
-
     
     private lazy var settingHeaderImage: UIImageView = {
         let view = UIImageView()
@@ -240,35 +247,77 @@ class MyPageViewController: UIViewController {
         label.textColor = .mainTextColor
         return label
     }()
-
     
     private lazy var settingTableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.tag = 1
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    private lazy var tableViewSectionView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .seperatorColor
+        return view
+    }()
+    
+    private lazy var csHeader: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private lazy var csHeaderImage: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "settings")
+        return view
+    }()
+    
+    private lazy var csHeaderLabel: UILabel = {
+        let label = UILabel()
+        label.font = .myBoldSystemFont(ofSize: 18)
+        label.text = "고객센터"
+        label.textColor = .mainTextColor
+        return label
+    }()
+    
+    private lazy var csTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.tag = 2
         return tableView
     }()
 
-    
-
+    private lazy var collectionView: RecommendCollectionView = {
+        let collectionView = RecommendCollectionView()
+        collectionView.bind(RestaurantListViewModel())
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
-        
-        
-         setupUI()
+
+        setupUI()
+        binding()
+    }
+    
+    func binding() {
+        collectionView.rx.modelSelected(RestaurantListData.self)
+            .subscribe(onNext: { model in
+                print(">> ", model.id)
+            }).disposed(by: disposeBag)
     }
     
     
     func setupNavigation() {
         self.navigationItem.rightBarButtonItems = [noticeBarButton]
-//        self.navigationController?.hidesBarsOnSwipe = true
     }
     
     func setupUI(){
-        
-        
         view.addSubview(scrollView)
         scrollView.snp.makeConstraints { make in
             make.top.trailing.leading.bottom.equalToSuperview()
@@ -276,12 +325,11 @@ class MyPageViewController: UIViewController {
         
         scrollView.addSubview(contentsView)
         contentsView.snp.makeConstraints { make in
-            make.height.equalTo(1400)
+            make.height.equalTo(1150)
             make.width.centerX.top.bottom.equalToSuperview()
         }
         
-        
-        [headerTitleLabel, userRecView, myActivityView, myActivitydiviedSectionView, recentMatzipView, recentMatzipSectionView, settingHeader, settingTableView].forEach{ contentsView.addSubview($0) }
+        [headerTitleLabel, userRecView, myActivityView, myActivitydiviedSectionView, recentMatzipView, recentMatzipSectionView, settingHeader, settingTableView, tableViewSectionView, csHeader, csTableView].forEach{ contentsView.addSubview($0) }
         
         headerTitleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(21.2)
@@ -412,7 +460,6 @@ class MyPageViewController: UIViewController {
             make.height.equalTo(30)
         }
 
-
         myActivitydiviedSectionView.snp.makeConstraints { make in
             make.top.equalTo(myActivityView.snp.bottom)
             make.leading.trailing.equalToSuperview()
@@ -424,20 +471,31 @@ class MyPageViewController: UIViewController {
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(myActivitydiviedSectionView.snp.bottom)
         }
+        
+        [collectionView, recentMatzipTitleLable].forEach { recentMatzipView.addSubview($0) }
+        recentMatzipTitleLable.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(15)
+            make.leading.equalToSuperview().inset(22.54)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(recentMatzipTitleLable.snp.bottom).offset(15)
+            make.height.equalTo(RecommendCollectionViewCell.height)
+        }
+        
         recentMatzipSectionView.snp.makeConstraints { make in
             make.top.equalTo(recentMatzipView.snp.bottom)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(10)
         }
-        
 
         settingHeader.snp.makeConstraints { make in
             make.top.equalTo(recentMatzipSectionView.snp.bottom)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(40)
+            make.height.equalTo(60)
         }
         
-
         [settingHeaderImage, settingHeaderLabel].forEach { settingHeader.addSubview($0) }
         settingHeaderImage.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
@@ -451,7 +509,35 @@ class MyPageViewController: UIViewController {
         settingTableView.snp.makeConstraints { make in
             make.top.equalTo(settingHeader.snp.bottom)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(170)
+            make.height.equalTo(45 * 2)
+        }
+        
+        tableViewSectionView.snp.makeConstraints { make in
+            make.top.equalTo(settingTableView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        
+        csHeader.snp.makeConstraints { make in
+            make.top.equalTo(tableViewSectionView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(60)
+        }
+        
+        [csHeaderImage, csHeaderLabel].forEach { csHeader.addSubview($0) }
+        csHeaderImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(18)
+        }
+        csHeaderLabel.snp.makeConstraints { make in
+            make.leading.equalTo(csHeaderImage.snp.trailing).offset(7)
+            make.centerY.equalToSuperview()
+        }
+        
+        csTableView.snp.makeConstraints { make in
+            make.top.equalTo(csHeader.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(45 * 5)
         }
     }
     
@@ -459,19 +545,56 @@ class MyPageViewController: UIViewController {
 
 extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        if tableView.tag == 1 {
+            return 2
+        }else {
+            return 5
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let cell = UITableViewCell()
         cell.indentationLevel = 1
-        cell.textLabel?.font = .mySystemFont(ofSize: 12)
-        cell.textLabel?.text = "\(indexPath.row)"
-        return cell
+        cell.textLabel?.font = .mySystemFont(ofSize: 16)
+        cell.accessoryType = .disclosureIndicator
+
+        if tableView.tag == 1 {
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "알림 설정"
+            case 1:
+                cell.textLabel?.text = "계정관리"
+            default:
+                cell.textLabel?.text = "알림 설정"
+            }
+            return cell
+            
+        } else {
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "공지사항"
+            case 1:
+                cell.textLabel?.text = "신고 / 문의하기"
+            case 2:
+                cell.textLabel?.text = "FAQ"
+            case 3:
+                cell.textLabel?.text = "약관 및 정책"
+            case 4:
+                cell.textLabel?.text = "개인정보 처리 방침"
+            default:
+                cell.textLabel?.text = "공지사항"
+            }
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 48
+        return 45
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
